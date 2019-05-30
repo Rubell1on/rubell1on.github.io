b41cd5095d40c6d9f90d3155402917ef98ee1d26
ablaev.ablaev <dmitry.ablaev@metahouse.ru>
2019-05-30 09:45:17 +0300

    code refactorting

--- JS/utils.js
+++ JS/utils.js
@@ -1,5 +1,138 @@
 const fs = require('fs');
 const request = require('request');
+const xlsx = require('node-xlsx');
+const cheerio = require('cheerio');
+const yandexApi = require('./yandexApi');
+
+const {yandex_token} = getEnvironment(process.env);
+const yandex = new yandexApi(yandex_token);
+
+async function refreshPage() {
+    await this.get('https://schedule-p.herokuapp.com/');
+    const date = {
+        y: new Date().getFullYear(),
+        m: new Date().getMonth() + 1,
+        d: new Date().getDate(),
+        h: new Date().getHours(),
+        M: new Date().getMinutes(),
+        s: new Date().getSeconds()
+    }
+    console.log(`Обновлено ${date.y}-${date.m}-${date.d} ${date.h}:${date.M}:${date.s}`);
+}
+
+async function parseSchedule() {
+    const list = await yandex.getList();
+    const dirList = yandex.getDirList(list);
+    const halfYear = this.getHalfYear();
+    const year = new Date().getFullYear();
+    const yearString = `${halfYear}-${year}`;
+
+    if (!dirList.includes(yearString)) {
+        await yandex.createFolder(yearString);
+
+        const html = await this.get('https://www.mirea.ru/education/schedule-main/schedule/');
+        const $ = cheerio.load(html);
+        const parsedHtml = $('#tab-content > li > div > h2, .uk-overflow-auto');
+
+        let str;
+        const obj = {};
+        parsedHtml.each((ind, el) => {
+            if (el.tagName === 'h2') {
+                str = cheerio.load(el).text();
+            } else {
+                if (str.match(/занятий/g)) {
+                    if (!obj.hasOwnProperty('pairs')) obj.pairs = [];
+                    obj.pairs.push(el);
+                } 
+                // else if (str.match(/сессии/g)) {
+                //     //TODO У бакалавров две отдельные таблицы под экзамены и под зачеты
+                //     if (!obj.hasOwnProperty('exams')) obj['exams'] = {};
+                //     Object.assign(obj['exams'], el);
+                // }
+            }
+        });
+
+        const links = obj.pairs.reduce((acc, el) => {
+            const temp = cheerio.load(el)('table > tbody > tr > td > a').map((ind, link) => link.attribs.href);
+            temp.each((ind, el) => acc.push(el));
+            
+            return acc;
+        }, [])
+            .filter(val => val.match(/.xls/g) && !val.match(/\d-kurs-(zaochniki|vecherniki)/g));
+        links.splice(-2, 2);
+        
+        const buffers = links.map(link => this.get(link, null), []);
+        const data = await Promise.all(buffers);
+
+        data.forEach(async (buffer, ind) => {
+            const reverse = links[ind].split("").reverse().join("");
+            const i = reverse.indexOf('/',0);
+            const name = reverse.slice(0, i).split("").reverse().join("");
+            const uploadLink = await yandex.getUploadLink(`/${yearString}/${name}/`);
+            const res = await yandex.putData(uploadLink.body.href, buffer);
+            if (res.res.statusCode === 201) {
+                console.log(`Файл №${ind} ${name} загружен`);
+            } else {
+                console.error(`Во время загрузки файла произошла ошибка: ${JSON.parse(res.body).message}`);
+            } 
+        }); 
+    }
+
+    const listOfFiles = await yandex.getList();
+    const filteredFiles = listOfFiles.body.items.filter(el => el.path.match(yearString));
+    console.log(`Получено ${filteredFiles.length} файлов`);
+    const buffers = filteredFiles.map(async (el) => {
+        const path = await yandex.getDowndloadLink(el.path);
+        const downloadData = await yandex.getData(path.body.href);
+        return downloadData.body;
+    }, []);
+    const data = await Promise.all(buffers);
+    //
+    return data.reduce((acc, el, ind) => {
+        const schedule = xlsx.parse(el);
+        const groupNames = this.getGroupNames(schedule);
+        const parsedSchedule = this.getSchedules(groupNames, schedule);
+        const temp = Object.entries(parsedSchedule).reduce((acc, group) => {
+            const name = group[0];
+            const schedule = group[1];
+
+            this.deleteUselessAttrs(schedule, ['num', 'begin', 'end', 'day', 'week']);
+            acc[name] = schedule;
+            
+            return acc;
+        }, {});
+        console.log(`Закончен парсинг ${ind + 1} объекта`);
+        return Object.assign(acc, temp);
+    }, {});
+}
+
+function getEnvironment(env) {
+    const SPREADSHEET_ID = env.SPREADSHEET_ID;
+    const creds = {
+        web: {
+            client_id: env.client_id,
+            project_id: env.project_id,
+            auth_uri: env.auth_uri,
+            token_uri: env.token_uri,
+            auth_provider_x509_cert_url: env.auth_provider_x509_cert_url,
+            client_secret: env.client_secret,
+            redirect_uris: env.redirect_uris
+        }
+    };
+
+    const token = {
+        access_token: env.access_token,
+        refresh_token: env.refresh_token,
+        scope: env.scope,
+        token_type: env.token_type,
+        expiry_date: Number(env.expiry_date), 
+    };
+
+    const yandex_token = env.yandex_token;
+
+    return {creds, token, SPREADSHEET_ID, yandex_token};
+}
+
 
 module.exports = {
     deleteUselessAttrs: function (obj, arr) {
@@ -88,32 +221,8 @@ module.exports = {
     isFileExists: function(path) {
         return fs.existsSync(path);
     },
-    getEnvironment: function(env) {
-        const SPREADSHEET_ID = env.SPREADSHEET_ID;
-        const creds = {
-            web: {
-                client_id: env.client_id,
-                project_id: env.project_id,
-                auth_uri: env.auth_uri,
-                token_uri: env.token_uri,
-                auth_provider_x509_cert_url: env.auth_provider_x509_cert_url,
-                client_secret: env.client_secret,
-                redirect_uris: env.redirect_uris
-            }
-        };
-
-        const token = {
-            access_token: env.access_token,
-            refresh_token: env.refresh_token,
-            scope: env.scope,
-            token_type: env.token_type,
-            expiry_date: Number(env.expiry_date), 
-        };
-
-        const yandex_token = env.yandex_token;
-
-        return {creds, token, SPREADSHEET_ID, yandex_token};
-    },
+    getEnvironment
+    ,
     get: function (uri) {
         const options = { uri };
     
@@ -173,5 +282,7 @@ module.exports = {
             }, {});
             return acc1;
         }, {});
-    }
+    },
+    parseSchedule,
+    refreshPage
 };
--- server.js
+++ server.js
@@ -1,15 +1,10 @@
-const xlsx = require('node-xlsx');
 const express = require('express');
 const utils = require('./JS/utils');
 const gOAuth = require('./JS/gOAuth');
-const yandexApi = require('./JS/yandexApi');
 const bodyParser = require('body-parser');
 const {google} = require('googleapis');
-const cheerio = require('cheerio');
 
-const {creds, token, SPREADSHEET_ID, yandex_token} = utils.getEnvironment(process.env);
-
-const yandex = new yandexApi(yandex_token);
+const {creds, token, SPREADSHEET_ID} = utils.getEnvironment(process.env);
 
 const oAuth2Client = new google.auth.OAuth2(creds.web.client_id, creds.web.client_secret, creds.web.redirect_uris);
 oAuth2Client.setCredentials(token);
@@ -17,115 +12,15 @@ oAuth2Client.setCredentials(token);
 const PORT = process.env.PORT || 3000;
 let schedules = {};
 
-async function parseSchedule() {
-    const list = await yandex.getList();
-    const dirList = yandex.getDirList(list);
-    const halfYear = utils.getHalfYear();
-    const year = new Date().getFullYear();
-    const yearString = `${halfYear}-${year}`;
-
-    if (!dirList.includes(yearString)) {
-        await yandex.createFolder(yearString);
-
-        const html = await utils.get('https://www.mirea.ru/education/schedule-main/schedule/');
-        const $ = cheerio.load(html);
-        const parsedHtml = $('#tab-content > li > div > h2, .uk-overflow-auto');
-
-        let str;
-        const obj = {};
-        parsedHtml.each((ind, el) => {
-            if (el.tagName === 'h2') {
-                str = cheerio.load(el).text();
-            } else {
-                if (str.match(/занятий/g)) {
-                    if (!obj.hasOwnProperty('pairs')) obj.pairs = [];
-                    obj.pairs.push(el);
-                } 
-                // else if (str.match(/сессии/g)) {
-                //     //TODO У бакалавров две отдельные таблицы под экзамены и под зачеты
-                //     if (!obj.hasOwnProperty('exams')) obj['exams'] = {};
-                //     Object.assign(obj['exams'], el);
-                // }
-            }
-        });
-
-        const links = obj.pairs.reduce((acc, el) => {
-            const temp = cheerio.load(el)('table > tbody > tr > td > a').map((ind, link) => link.attribs.href);
-            temp.each((ind, el) => acc.push(el));
-            
-            return acc;
-        }, [])
-            .filter(val => val.match(/.xls/g) && !val.match(/\d-kurs-(zaochniki|vecherniki)/g));
-        links.splice(-2, 2);
-        
-        const buffers = links.map(link => utils.get(link, null), []);
-        const data = await Promise.all(buffers);
-
-        data.forEach(async (buffer, ind) => {
-            const reverse = links[ind].split("").reverse().join("");
-            const i = reverse.indexOf('/',0);
-            const name = reverse.slice(0, i).split("").reverse().join("");
-            const uploadLink = await yandex.getUploadLink(`/${yearString}/${name}/`);
-            const res = await yandex.putData(uploadLink.body.href, buffer);
-            if (res.res.statusCode === 201) {
-                console.log(`Файл №${ind} ${name} загружен`);
-            } else {
-                console.error(`Во время загрузки файла произошла ошибка: ${JSON.parse(res.body).message}`);
-            } 
-        }); 
-    }
-
-    const listOfFiles = await yandex.getList();
-    const filteredFiles = listOfFiles.body.items.filter(el => el.path.match(yearString));
-    console.log(`Получено ${filteredFiles.length} файлов`);
-    const buffers = filteredFiles.map(async (el) => {
-        const path = await yandex.getDowndloadLink(el.path);
-        const downloadData = await yandex.getData(path.body.href);
-        return downloadData.body;
-    }, []);
-    const data = await Promise.all(buffers);
-    //
-    schedules = data.reduce((acc, el, ind) => {
-        const schedule = xlsx.parse(el);
-        const groupNames = utils.getGroupNames(schedule);
-        const parsedSchedule = utils.getSchedules(groupNames, schedule);
-        const temp = Object.entries(parsedSchedule).reduce((acc, group) => {
-            const name = group[0];
-            const schedule = group[1];
-
-            utils.deleteUselessAttrs(schedule, ['num', 'begin', 'end', 'day', 'week']);
-            acc[name] = schedule;
-            
-            return acc;
-        }, {});
-        console.log(`Закончен парсинг ${ind + 1} объекта`);
-        return Object.assign(acc, temp);
-    }, {});
-}
-
- 
-
 app = express();
 
-parseSchedule()
-    .then(() => {
+utils.parseSchedule()
+    .then((data) => {
+        schedules = data;
         app.listen(PORT, () => {
             console.log(`Сервер запущен и ожидает запросы по ${PORT}`);
             setInterval(() => {
-                refreshPage().catch(e => console.log(e));
-
-                async function refreshPage() {
-                    await utils.get('https://schedule-p.herokuapp.com/');
-                    const date = {
-                        y: new Date().getFullYear(),
-                        m: new Date().getMonth() + 1,
-                        d: new Date().getDate(),
-                        h: new Date().getHours(),
-                        M: new Date().getMinutes(),
-                        s: new Date().getSeconds()
-                    }
-                    console.log(`Обновлено ${date.y}-${date.m}-${date.d} ${date.h}:${date.M}:${date.s}`);
-                }
+                utils.refreshPage().catch(e => console.log(e));
             }, 300000);
         });
     })
